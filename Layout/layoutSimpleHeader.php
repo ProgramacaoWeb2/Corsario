cab198fd216a4b4097d99e994fcc08397f4b664b
Mauricio Andreolla de Brito <mauricioandreolla0000@gmail.com>
2021-04-21 18:22:03 -0300

    Update layoutSimpleHeader.php

--- Layout/layoutSimpleHeader.php
+++ Layout/layoutSimpleHeader.php
@@ -3,21 +3,27 @@
 
 
 <head>
-	<meta charset="UTF-8">
-    
-	<title><?php echo $page_title; ?></title>
+    <meta charset="UTF-8">
+
+    <title><?php echo $page_title; ?></title>
     <link href="Content/fonts/Roboto" rel="stylesheet">
     <link rel="stylesheet" href="Content/css/app.css" />
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     
-    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+
 </head>
 
 <body>
-	<header id="appHeader">
+    <header id="appHeader">
         <div id="appBrand">
             <img src="Content/images/appBrand.png" alt="">
         </div>
-	</header>
+    </header>
 
     <div id="appBody">
