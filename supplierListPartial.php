cb74d053e7eaca55f8644eef6bc514cba6a5771c
Mauricio Andreolla de Brito <mauricioandreolla0000@gmail.com>
2021-04-25 01:05:41 -0300

    Consulta por id e name Fornecedor

--- supplierListPartial.php
+++ supplierListPartial.php
@@ -1,18 +1,16 @@
 <?php
-$page_title = "Pesquisa Detalhada";
+$page_title = "Pesquisa detalhada de Fornecedores";
 
 include_once("Layout/layoutHeader.php");
-include_once("DbFactory.php");
+include_once('DbFactory.php');
 
 
+$supliers = NULL;
+
+$supliers =  $db->Fornecedor()->getTodos();
 
 
-$productList = NULL;
-
-$productList = $db->Produto()->getTodos();
-
-
-if ($productList != NULL) {
+if ($supliers != NULL) {
 
 ?>
 
@@ -23,6 +21,8 @@ if ($productList != NULL) {
                 <th scope="col">#</th>
                 <th scope="col">Nome</th>
                 <th scope="col">Descrição</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">E-mail</th>
                 <th>
             </tr>
         </thead>
@@ -30,14 +30,16 @@ if ($productList != NULL) {
 
 
         <?php
-        foreach ($productList as $product) { ?>
+        foreach ($supliers as $supplier) { ?>
 
 
 
             <tr>
-                <td><?= $product->getId() ?></td>
-                <td><?= $product->getNome() ?></td>
-                <td><?= $product->getDescricao() ?></td>
+                <td><?= $supplier->getId() ?></td>
+                <td><?= $supplier->getNome() ?></td>
+                <td><?= $supplier->getDescricao() ?></td>
+                <td><?= $supplier->getTelefone() ?></td>
+                <td><?= $supplier->getEmail() ?></td>
 
                 <td>
 
@@ -47,8 +49,8 @@ if ($productList != NULL) {
                             Opções
                         </button>
                         <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            <a class="dropdown-item" href="productEdit.php?id=<?= $product->getId() ?>"> Editar Produto</a>
-                            <a class="dropdown-item btn-delete-user" href="productDelete.php?id=<?= $product->getId() ?>"> Deletar usuário</a>
+                            <a class="dropdown-item" href="supplierEdit.php?id=<?= $supplier->getId() ?>"> Editar Fornecedor</a>
+                            <a class="dropdown-item btn-delete-user" href="supplierDelete.php?id=<?= $supplier->getId() ?>"> Deletar Fornecedor</a>
                         </div>
                     </div>
 
@@ -67,5 +69,7 @@ if ($productList != NULL) {
     </table>
 <?php
 } else { ?>
+
+
 
 <?php } ?>
