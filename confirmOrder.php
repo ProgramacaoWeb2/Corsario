7e6ecf0fffe1417d739149e4654ccab65da1496d
Gabriel Rodrigues <gsrodrigues2@ucs.br>
2021-06-19 20:43:24 -0300

    Update confirmOrder.php

--- confirmOrder.php
+++ confirmOrder.php
@@ -5,7 +5,7 @@
     $listaProdutos = json_decode($_SESSION["SessionCart"]); // [ {idProduto: 1, qtd: 2}, {idProduto: 3, qtd: 1}]
     $idUsuario = $_SESSION["idUsuario"];
 
-    $cu = 0;
+    //Chamar create do pedido
 
 
 ?>
